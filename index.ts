491a09174b8fb3fefa5f121784c72e024dc16141
Thomas Nordentoft <thomas@nordentoft.dk>
2025-06-03 19:14:12 +0200

    Remove web-streams-polyfill

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@
 // import { polyfill } from 'react-native-polyfill-globals/src/fetch';
 // polyfill();
 
-import { ReadableStream as ReadableStreamPolyfill } from 'web-streams-polyfill/dist/ponyfill';
 // // @ts-ignore
 
 import {fetch as expoFetch} from 'expo/fetch';
@@ -14,7 +13,6 @@ import 'text-encoding';
 if(Platform.OS !== 'web') {
     
     globalThis.fetch = expoFetch;
-    globalThis.ReadableStream = ReadableStreamPolyfill;
 }
 
 // Your existing app entry point
