e44dfd2d2e12293bb0cff77796b6c937152a960c
Thomas Nordentoft <thomas@nordentoft.dk>
2025-06-10 13:14:10 +0200

    Linux build working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+  // Set environment variables to fix WebKit rendering issues on Linux
+  std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+  std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
   //app_lib::run();
   tauri::Builder::default()
     .plugin(tauri_plugin_shell::init())
