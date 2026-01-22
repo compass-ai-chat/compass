e741c66c92b4ce15e3f7d35212056c84792d3fc8
Thomas Nordentoft <thomas@nordentoft.dk>
2026-01-22 22:44:55 +0100

    Tidy

--- src/services/chat/ChatProviderFactory.ts
+++ src/services/chat/ChatProviderFactory.ts
@@ -1,12 +1,6 @@
 import { Model, Provider } from '@/src/types/core';
 import { ChatProvider } from '@/src/types/chat';
 import { OllamaProvider } from './providers/OllamaProvider';
-import { OpenAIProvider } from './providers/OpenAIProvider';
-import { AnthropicProvider } from './providers/AnthropicProvider';
-import { GroqProvider } from './providers/GroqProvider';
-import { CerebrasProvider } from './providers/CerebrasProvider';
-import { MistralProvider } from './providers/MistralProvider';
-import { XAIProvider } from './providers/XAIProvider';
 import { PolarisProvider } from './providers/PolarisProvider';
 export class ChatProviderFactory {
   static getProvider(provider: Provider): ChatProvider {
